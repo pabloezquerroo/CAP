d963a3d26ba80ab9d1ed98dbdbe7f5328efe9e76
pabloezquerroo <pabloezquerro2000@gmail.com>
2024-10-23 13:48:30 +0200

    lab

--- src/lab1/Intrinsics/stencil.c
+++ src/lab1/Intrinsics/stencil.c
@@ -1,5 +1,7 @@
 #include "stencil.h"
+#include "immintrin.h"
 
+/*
 void ApplyStencil(unsigned char *img_in, unsigned char *img_out, int width, int height) {
   
 	short val;
@@ -20,6 +22,49 @@ void ApplyStencil(unsigned char *img_in, unsigned char *img_out, int width, int 
 
 			img_out[i*width + j] = (unsigned char)(val_out);
 		}
+}
+*/
+
+
+void ApplyStencil(unsigned char *img_in, unsigned char *img_out, int width, int height) {
+    for (int i = 1; i < height - 1; i++) {
+        for (int j = 1; j < width - 1; j += 32) { // Procesar 32 píxeles a la vez
+
+            // Cargar la vecindad de 3x3 para 32 píxeles
+            __m256i val_UL = _mm256_loadu_si256((__m256i*)&img_in[(i-1) * width + j-1]);
+            __m256i val_U = _mm256_loadu_si256((__m256i*)&img_in[(i-1) * width + j]);
+            __m256i val_UR = _mm256_loadu_si256((__m256i*)&img_in[(i-1) * width + j+1]);
+            __m256i val_L = _mm256_loadu_si256((__m256i*)&img_in[(i) * width + j-1]);
+            __m256i val_C = _mm256_loadu_si256((__m256i*)&img_in[(i) * width + j]);  // Este es el píxel central
+            __m256i val_R = _mm256_loadu_si256((__m256i*)&img_in[(i) * width + j+1]);
+            __m256i val_DL = _mm256_loadu_si256((__m256i*)&img_in[(i+1) * width + j-1]);
+            __m256i val_D = _mm256_loadu_si256((__m256i*)&img_in[(i+1) * width + j]);
+            __m256i val_DR = _mm256_loadu_si256((__m256i*)&img_in[(i+1) * width + j+1]);
+
+            // Inicializa el resultado a 0
+            __m256i result = _mm256_setzero_si256();
+
+            // Sumar y restar las vecindades según el stencil
+            result = _mm256_sub_epi16(result, val_UL);
+            result = _mm256_sub_epi16(result, val_U);
+            result = _mm256_sub_epi16(result, val_UR);
+            result = _mm256_sub_epi16(result, val_L);
+            result = _mm256_add_epi16(result, _mm256_slli_epi16(val_C, 3)); // 7 * val_C
+            result = _mm256_sub_epi16(result, val_R);
+            result = _mm256_sub_epi16(result, val_DL);
+            result = _mm256_sub_epi16(result, val_D);
+            result = _mm256_sub_epi16(result, val_DR);
+
+            // Saturar el resultado a [0, 255]
+            result = _mm256_min_epi16(_mm256_max_epi16(result, _mm256_set1_epi16(0)), _mm256_set1_epi16(255));
+
+            // Empacar los resultados de 16 bits a 8 bits
+            __m256i packed_result = _mm256_packus_epi16(result, result);
+
+            // Almacenar el resultado en la imagen de salida
+            _mm256_storeu_si256((__m256i*)&img_out[i * width + j], packed_result);
+        }
+    }
 }
 
 void CopyImage(unsigned char *img_in, unsigned char *img_out, int width, int height) {
